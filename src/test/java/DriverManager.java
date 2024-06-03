317a0129eb0fe2f89b4e67cafecd08b3074e0b0b
NehallBaig <nehallbaig@gmail.com>
2024-06-03 22:33:36 +0500

    Training 3 Session 1 Explain Maven, Objects and Framework

---
+++ src/test/java/DriverManager.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class DriverManager {
+
+    public static void main(String[] args) {
+
+        // chromeDriver is Reference of ChromeDriver's Object
+        ChromeDriver chromeDriver = new ChromeDriver();
+
+        // Get function is used to navigate to the url
+        chromeDriver.get("https://www.google.com");
+
+        // edgeDriver is Reference of EdgeDriver's Object
+        EdgeDriver edgeDriver = new EdgeDriver();
+        edgeDriver.get("https://www.youtube.com");
+
+        chromeDriver.get("https://www.youtube.com");
+
+
+
+
+
+
+        //System.out.println("HI helo: ) ");
+    }
+}
